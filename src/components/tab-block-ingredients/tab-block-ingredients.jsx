1238871ee90262477adbd151e659957ec20b9f80
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-12-25 22:32:10 +0500

    fix: удалён лишний атрибут

--- src/components/tab-block-ingredients/tab-block-ingredients.jsx
+++ src/components/tab-block-ingredients/tab-block-ingredients.jsx
@@ -9,13 +9,13 @@ const TabBlockIngredients = () => {
 
   return (
     <div className={TabBlockStyles.tab}>
-      <Link to='buns' activeClass='active' spy={true} smooth={true} duration={700} containerId='section-blocks'>
+      <Link to='buns' spy={true} smooth={true} duration={700} containerId='section-blocks'>
         <Tab value='buns' active={current === 'buns'} onClick={setCurrent}>Булки</Tab>
       </Link>
-      <Link to='sauces' activeClass='active' spy={true} smooth={true} duration={700} containerId='section-blocks'>
+      <Link to='sauces' spy={true} smooth={true} duration={700} containerId='section-blocks'>
         <Tab value='sauces' active={current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
       </Link>
-      <Link to='fillings' activeClass='active' spy={true} smooth={true} duration={700} containerId='section-blocks'>
+      <Link to='fillings' spy={true} smooth={true} duration={700} containerId='section-blocks'>
         <Tab value='fillings' active={current === 'fillings'} onClick={setCurrent}>Начинки</Tab>
       </Link>
     </div>
