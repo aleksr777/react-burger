50cb5f724b35d336499f4ccc3383af482bce4853
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-01-18 21:22:01 +0500

    refactor: настройки Redux Toolkit

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,17 @@
 import './index.css';
 import React from 'react';
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { createRoot } from 'react-dom/client';
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { rootReducer } from './services/reducers/root-reducer';
 
-// Усилитель
-const actionLogger = store => next => action => {
-  // Выводим в консоль время события и его содержание
-  console.log(`${new Date().getTime()} | Action: ${JSON.stringify(action)}`);
-  // Передаём событие «по конвейеру» дальше
-  return next(action);
-};
-
-// Усилитель
-const errorLogger = store => next => action => {
-  if (action.type === 'SOMETHING_FAILED') {
-    console.error(`Произошла ошибка: ${JSON.stringify(action)}`)
-  }
-  return next(action);
-};
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk, actionLogger, errorLogger));
-
-const store = createStore(rootReducer, enhancer);
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+})
 
 const root = createRoot(document.getElementById('root'));
 
