5ea925d65f20760d6dc529988076bac0ea3e6b84
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-02-20 11:08:34 +0500

    feat: установлен react-router-dom, создана файловая структура для будующих страниц.

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { NotFound404 } from './not-found/not-found';
+import { HomePage } from './home/home';
+import { loginPage } from './login/login';
+import { registerPage } from './register/register';
+import { forgotPasswordPage } from './forgot-password/forgot-password';
+import { resetPasswordPage } from './reset-password/reset-password';
+import { profilePage } from './profile/profile';
+import { ingredientPage } from './ingredient/ingredient';
+
+export { NotFound404, HomePage, loginPage, registerPage, forgotPasswordPage, resetPasswordPage, profilePage, ingredientPage };
