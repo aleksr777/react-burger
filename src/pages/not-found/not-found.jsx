16b82610c70c9dcb31ff8201bdef328e47b2ebdb
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-03-20 18:19:42 +0500

    refactor: удалена лишняя оболочка у компонента NotFound404

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,5 +1,4 @@
 import notFoundStyles from './not-found.module.css';
-import React from 'react';
 import { Link } from 'react-router-dom';
 import ImgPath from '../../images/ban.svg';
 
@@ -7,20 +6,18 @@ import ImgPath from '../../images/ban.svg';
 const NotFound404 = () => {
 
   return (
-    <div className={notFoundStyles.wrapper}>
-      <div className={notFoundStyles.container}>
-        <div className={notFoundStyles.textBox}>
-          <p className={`text text_type_main-large ${notFoundStyles.text}`}>Упс... Страница не найдена!</p>
-          <p className={`text text_type_digits-large ${notFoundStyles.text}`}>404 Error</p>
-          <p className={`text text_type_main-medium ${notFoundStyles.text}`}>
-            Проверьте адрес ссылки или перейдите на <Link to='/' className={notFoundStyles.link}>главную страницу</Link>
-          </p>
-        </div>
-        <div className={notFoundStyles.pictureBox}>
-          <picture className={notFoundStyles.imageBox}>
-            <img className={notFoundStyles.image} src={ImgPath} alt="Страница не найдена" />
-          </picture>
-        </div>
+    <div className={notFoundStyles.container}>
+      <div className={notFoundStyles.textBox}>
+        <p className={`text text_type_main-large ${notFoundStyles.text}`}>Упс... Страница не найдена!</p>
+        <p className={`text text_type_digits-large ${notFoundStyles.text}`}>404 Error</p>
+        <p className={`text text_type_main-medium ${notFoundStyles.text}`}>
+          Проверьте адрес ссылки или перейдите на <Link to='/' className={notFoundStyles.link}>главную страницу</Link>
+        </p>
+      </div>
+      <div className={notFoundStyles.pictureBox}>
+        <picture className={notFoundStyles.imageBox}>
+          <img className={notFoundStyles.image} src={ImgPath} alt="Страница не найдена" />
+        </picture>
       </div>
     </div>
   );
