7121153e6554c0eb6531f065467a538033a039ce
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-06-20 22:54:47 +0500

    refactor: переформатировал и типизировал компоненты CounterItem, ErrorInfo.

--- src/components/error-info/error-info.tsx
+++ src/components/error-info/error-info.tsx
@@ -1,24 +1,21 @@
 import stylesErrorInfo from './error-info.module.css';
-import PropTypes from 'prop-types';
 import ImgPath from '../../images/ban.svg';
 
-const ErrorInfo = ({ title }) => {
+type Props = {
+  title: string;
+};
+
+const ErrorInfo = ({ title }: Props) => {
   return (
     <div className={stylesErrorInfo.container}>
       <div className={stylesErrorInfo.textBox}>
-        <p className={stylesErrorInfo.title}>
-          {title}
-        </p>
+        <p className={stylesErrorInfo.title}>{title}</p>
       </div>
       <picture className={stylesErrorInfo.imageBox}>
-        <img className={stylesErrorInfo.image} src={ImgPath} alt="Ошибка." />
+        <img className={stylesErrorInfo.image} src={ImgPath} alt='Ошибка.' />
       </picture>
     </div>
   );
-};
-
-ErrorInfo.propTypes = {
-  title: PropTypes.string.isRequired
 };
 
 export default ErrorInfo;
