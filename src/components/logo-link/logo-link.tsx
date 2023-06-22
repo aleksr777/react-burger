a55295a3e480d4ed86103f285fbda026d89c57a4
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-06-22 22:47:38 +0500

    refactor: типизирован компонент LogoLink

---
+++ src/components/logo-link/logo-link.tsx
@@ -0,0 +1,33 @@
+import stylesLogoLink from './logo-link.module.css'
+import { useState, useEffect } from 'react'
+import { useLocation, useMatch, Link } from 'react-router-dom'
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+
+
+const LogoLink = () => {
+
+  const defaultStyle: string = stylesLogoLink.link
+  const activeStyle: string = `${ stylesLogoLink.link } ${ stylesLogoLink.link_active }`
+
+  const match = useMatch( '/' )
+  const location = useLocation()
+
+  const [ isActiveLink, setIsActiveLink ] = useState<boolean>( false )
+
+  useEffect( () => {
+    match ? setIsActiveLink( true ) : setIsActiveLink( false )
+  }, [ location.pathname, match ] )
+
+  return (
+    <Link
+      className={ isActiveLink ? activeStyle : defaultStyle }
+      tabIndex={ isActiveLink ? -1 : undefined }
+      to="/"
+      draggable={ false }
+    >
+      <Logo />
+    </Link>
+  )
+}
+
+export default LogoLink
