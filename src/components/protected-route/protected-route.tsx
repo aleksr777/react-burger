1acc5cc66739dc98b8043a9249f5cbeb77f1d6a2
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-06-25 19:00:31 +0500

    refactor: типизирован компонент ProtectedRouteElement

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { checkAuth } from '../../services/authorization/check-auth'
+import { deleteAuthData } from '../../services/authorization/auth-actions'
+import { getAuthState } from '../../utils/selectors'
+import { AuthStateType } from '../../types/types'
+
+type Props = {
+  children: React.ReactNode
+  forUnauthUser: boolean
+}
+
+
+const ProtectedRouteElement = ( { children, forUnauthUser }: Props ) => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const fromPage: string = location.state || '/'
+
+  const { isSuccess, user }: AuthStateType = useSelector( getAuthState )
+
+  let isAuth: boolean = checkAuth( isSuccess, user.email )
+
+  useEffect( () => {
+    if ( !isAuth ) {
+      dispatch( deleteAuthData() as any )
+    }
+  }, [ isAuth, isSuccess, user ] )
+
+  useEffect( () => {
+    if ( !isAuth && !forUnauthUser ) {
+      return navigate( '/login', { state: location.pathname } )
+    }
+    else if ( isAuth && forUnauthUser ) {
+      return navigate( fromPage, { replace: true } )
+    }
+  }, [ isAuth, forUnauthUser, isSuccess, user ] )
+
+  return (
+    ( ( isAuth && !forUnauthUser ) || ( !isAuth && forUnauthUser ) ) && children
+  )
+}
+
+export default ProtectedRouteElement
