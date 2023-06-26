16c70375a79ecca073e82704fb9136c044adbd5d
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-06-26 12:04:36 +0500

    refactor: поправки в IngredientDetailsPage

--- src/pages/ingredient-details/ingredient-details.tsx
+++ src/pages/ingredient-details/ingredient-details.tsx
@@ -1,40 +1,40 @@
-import styles from './ingredient-details.module.css';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useLocation, useNavigate } from "react-router-dom";
-import { getIngredientInfo } from '../../services/ingredients-data/ingredients-data-actions';
-import IngredientDetailsLayout from '../../components/ingredient-details-layout/ingredient-details-layout';
-import { getIngredientDetailsState } from '../../utils/selectors';
-import { IngredientInfoType } from '../../types/types';
+import styles from './ingredient-details.module.css'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useLocation, useNavigate } from "react-router-dom"
+import { getIngredientInfo } from '../../services/ingredients-data/ingredients-data-actions'
+import IngredientDetailsLayout from '../../components/ingredient-details-layout/ingredient-details-layout'
+import { getIngredientDetailsState } from '../../utils/selectors'
+import { IngredientInfoType } from '../../types/types'
 
 /* Реализовал этот компонент так, чтобы можно было получить информацию по ингредиенту, если переходить на страницу по внешней ссылке*/
 const IngredientDetailsPage = () => {
 
-  const { pathname } = useLocation();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const { pathname }: { pathname: string } = useLocation()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   //достаём id из строки адреса
-  const id = pathname.split('/').pop();
+  const id: string | undefined = pathname.split( '/' ).pop()
 
-  const { ingredient }: { ingredient: IngredientInfoType } = useSelector( getIngredientDetailsState );
+  const { ingredient }: { ingredient: IngredientInfoType } = useSelector( getIngredientDetailsState )
 
-  function goToNotFoundPage() {
-    navigate('/not-found-page', { replace: true });
+  function goToNotFoundPage () {
+    navigate( '/not-found-page', { replace: true } )
   };
 
-  useEffect(() => {
-    if (!ingredient || !ingredient._id || ingredient.path !== pathname) {
-      dispatch(getIngredientInfo(goToNotFoundPage, id, pathname) as any)
+  useEffect( () => {
+    if ( !ingredient || !ingredient._id || ingredient.path !== pathname ) {
+      dispatch( getIngredientInfo( goToNotFoundPage, id, pathname ) as any )
     }
-  }, []);
+  }, [] )
 
   return (
     ingredient &&
-    <div className={styles.container}>
-      <IngredientDetailsLayout ingredient={ingredient} titleAlign='center' />
+    <div className={ styles.container }>
+      <IngredientDetailsLayout ingredient={ ingredient } titleAlign='center' />
     </div>
   )
-};
+}
 
-export default IngredientDetailsPage;
+export default IngredientDetailsPage
