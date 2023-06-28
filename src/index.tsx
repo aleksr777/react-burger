a8df99d7a8a1aa29885ef57001de967e2c73946e
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-06-28 09:09:40 +0500

    refactor: Создал отдельный файл store.ts, перенёс в него логику создания хранилища и создал базовые настройки типизации для хуков useDispatch и useSelector.

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,11 @@
 import './index.css'
 import React from 'react'
-import ReactDOM from "react-dom/client"
-import App from './components/app/app'
-import rootReducer from './services/root-reducer'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { BrowserRouter } from 'react-router-dom'
-import { configureStore } from '@reduxjs/toolkit'
-import { socketMiddleware } from './services/socketMiddleware/socketMiddleware'
-import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist'
-import { urlFeedOrders, urlProfileOrders } from './utils/api'
-import { feedOrdersActions } from './services/feed-all-orders/feed-all-orders-actions'
-import { profileOrdersActions } from './services/profile-orders/profile-orders-actions'
-
-
-const socketMiddlewareFeedOrders = socketMiddleware( urlFeedOrders, feedOrdersActions )
-const socketMiddlewareProfileOrders = socketMiddleware( urlProfileOrders, profileOrdersActions )
-
-const store = configureStore( {
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== 'production',
-  middleware: ( getDefaultMiddleware ) => getDefaultMiddleware( {
-    serializableCheck: {
-      ignoredActions: [ FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER ],
-    },
-  } ).concat( socketMiddlewareFeedOrders, socketMiddlewareProfileOrders )
-} )
-
-const persistor = persistStore( store )
+import ReactDOM from "react-dom/client"
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import App from './components/app/app'
 
 const root = ReactDOM.createRoot( document.getElementById( "root" ) as HTMLElement )
 
@@ -42,6 +20,4 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-export { store };
 
---
+++ src/store/store.ts
@@ -0,0 +1,28 @@
+import rootReducer from '../services/root-reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { socketMiddleware } from '../services/socketMiddleware/socketMiddleware';
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import { urlFeedOrders, urlProfileOrders } from '../utils/api';
+import { feedOrdersActions } from '../services/feed-all-orders/feed-all-orders-actions';
+import { profileOrdersActions } from '../services/profile-orders/profile-orders-actions';
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+const socketMiddlewareFeedOrders = socketMiddleware(urlFeedOrders, feedOrdersActions);
+const socketMiddlewareProfileOrders = socketMiddleware(urlProfileOrders, profileOrdersActions);
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(socketMiddlewareFeedOrders, socketMiddlewareProfileOrders),
+} );
+
+
+export const persistor = persistStore(store);
